98b14c0c68eca091dcd198e026714b8d2591debd
Florin Große <florin.grosse@idot-digital.de>
2025-05-05 00:11:56 +0200

    Style overhaul for strat list

--- app/(app)/strats/page.tsx
+++ app/(app)/strats/page.tsx
@@ -1,43 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import StratsDB from "@/src/db";
+import { Edit } from "lucide-react";
+import Link from "next/link";
 
 export default async function StratsPage() {
   const strats = StratsDB.list();
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>id</td>
-          <td>map</td>
-          <td>site</td>
-          <td>name</td>
-          <td>preview</td>
-          <td>edit</td>
-        </tr>
-      </thead>
-      <tbody>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Map</TableHead>
+          <TableHead>Site</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Preview</TableHead>
+          <TableHead>Edit</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
         {strats.map((strat) => (
-          <tr key={strat.id}>
-            <td>{strat.id}</td>
-            <td>{strat.map}</td>
-            <td>{strat.site}</td>
-            <td>{strat.name}</td>
-            <td>
-              <a
+          <TableRow key={strat.id}>
+            <TableCell>{strat.map}</TableCell>
+            <TableCell>{strat.site}</TableCell>
+            <TableCell>{strat.name}</TableCell>
+            <TableCell>
+              <Link
                 href={strat.previewURL}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Preview
-              </a>
-            </td>
-            <td>
-              <a href={strat.editURL} target="_blank" rel="noopener noreferrer">
-                Edit
-              </a>
-            </td>
-          </tr>
+                <Button variant="outline" className="w-full cursor-pointer">
+                  Preview
+                </Button>
+              </Link>
+            </TableCell>
+            <TableCell>
+              <Link
+                href={strat.editURL}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline" className="w-full cursor-pointer">
+                  <Edit />
+                  Edit
+                </Button>
+              </Link>
+            </TableCell>
+          </TableRow>
         ))}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 }
