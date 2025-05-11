1015af0174c5cab720e79072590b884155fc4ab2
Florin Große <florin.grosse@idot-digital.de>
2025-05-11 20:01:14 +0200

    Finished layout style for editor

--- components/StratEditor.tsx
+++ components/StratEditor.tsx
@@ -6,12 +6,13 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/src/utils";
-import { MapPin, Save, Shield, User, X } from "lucide-react";
+import { MapPin, Save, Shield, User, X, Menu } from "lucide-react";
 import OperatorIcon from "@/components/OperatorIcon";
 import DEFENDERS from "@/src/static/operator";
 import ATTACKERS from "@/src/static/operator";
 import MAPS from "@/src/static/maps";
 import { Badge } from "./ui/badge";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 interface StratEditorProps {
   strat: StratDrawing;
@@ -26,90 +27,110 @@ export function StratEditor({ strat }: StratEditorProps) {
     [strat.map]
   );
 
+  const sidebar = (
+    <Tabs defaultValue="operators" className="p-4 pb-0 h-screen flex-1">
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger value="operators">Operators</TabsTrigger>
+        <TabsTrigger value="gadgets">Gadgets</TabsTrigger>
+        <TabsTrigger value="markers">Markers</TabsTrigger>
+      </TabsList>
+      <TabsContent value="operators" className="relative">
+        <div className="h-full absolute inset-0">
+          <ScrollArea className="h-full">
+            <div className="grid grid-cols-2 gap-2 items-center">
+              <Badge className="sticky top-0 w-full col-span-2">
+                Defenders
+              </Badge>
+              {DEFENDERS.map((op) => (
+                <div
+                  key={op.name}
+                  className="flex cursor-move items-center gap-2 rounded-md border p-2 hover:bg-accent"
+                >
+                  <OperatorIcon op={op} />
+                  <span className="text-sm">{op.name}</span>
+                </div>
+              ))}
+              <Badge className="sticky top-0 w-full col-span-2">
+                Attackers
+              </Badge>
+              {ATTACKERS.map((op) => (
+                <div
+                  key={op.name}
+                  className="flex cursor-move items-center gap-2 rounded-md border p-2 hover:bg-accent"
+                >
+                  <OperatorIcon op={op} />
+                  <span className="text-sm">{op.name}</span>
+                </div>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </TabsContent>
+      <TabsContent value="gadgets" className="mt-4">
+        <ScrollArea className="h-[calc(100vh-200px)]">
+          <div className="grid grid-cols-2 gap-2"></div>
+        </ScrollArea>
+      </TabsContent>
+      <TabsContent value="markers" className="mt-4">
+        <ScrollArea className="h-[calc(100vh-200px)]">
+          <div className="grid grid-cols-2 gap-2"></div>
+        </ScrollArea>
+      </TabsContent>
+    </Tabs>
+  );
+
   return (
-    <div className="h-screen w-screen overflow-hidden grid grid-cols-[1fr_4fr]">
+    <div className="h-screen w-screen overflow-hidden xl:grid xl:grid-cols-[1fr_4fr]">
       {/* Toolbar */}
       <div className="flex flex-row">
-        <Tabs defaultValue="operators" className="p-4 pb-0 h-screen flex-1">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="operators">Operators</TabsTrigger>
-            <TabsTrigger value="gadgets">Gadgets</TabsTrigger>
-            <TabsTrigger value="markers">Markers</TabsTrigger>
-          </TabsList>
-          <TabsContent value="operators" className="relative">
-            <div className="h-full absolute inset-0">
-              <ScrollArea className="h-full">
-                <div className="grid grid-cols-2 gap-2 items-center">
-                  <Badge className="sticky top-0 w-full col-span-2">
-                    Defenders
-                  </Badge>
-                  {DEFENDERS.map((op) => (
-                    <div
-                      key={op.name}
-                      className="flex cursor-move items-center gap-2 rounded-md border p-2 hover:bg-accent"
-                    >
-                      <OperatorIcon op={op} />
-                      <span className="text-sm">{op.name}</span>
-                    </div>
-                  ))}
-                  <Badge className="sticky top-0 w-full col-span-2">
-                    Attackers
-                  </Badge>
-                  {ATTACKERS.map((op) => (
-                    <div
-                      key={op.name}
-                      className="flex cursor-move items-center gap-2 rounded-md border p-2 hover:bg-accent"
-                    >
-                      <OperatorIcon op={op} />
-                      <span className="text-sm">{op.name}</span>
-                    </div>
-                  ))}
-                </div>
-              </ScrollArea>
-            </div>
-          </TabsContent>
-          <TabsContent value="gadgets" className="mt-4">
-            <ScrollArea className="h-[calc(100vh-200px)]">
-              <div className="grid grid-cols-2 gap-2"></div>
-            </ScrollArea>
-          </TabsContent>
-          <TabsContent value="markers" className="mt-4">
-            <ScrollArea className="h-[calc(100vh-200px)]">
-              <div className="grid grid-cols-2 gap-2"></div>
-            </ScrollArea>
-          </TabsContent>
-        </Tabs>
-        <Separator orientation="vertical" className="mx-2" />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="xl:hidden absolute top-2 left-2 z-10"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-[300px] p-0" hideClose>
+            <SheetTitle className="sr-only">Toolbar</SheetTitle>
+            {sidebar}
+          </SheetContent>
+        </Sheet>
+
+        {/* Desktop Sidebar */}
+        <div className="hidden xl:block w-[300px]">{sidebar}</div>
+        <Separator orientation="vertical" className="mx-2 hidden xl:block" />
       </div>
 
       {/* Canvas */}
-      <div
-        className="relative overflow-hidden self-center"
-        style={
-          map && map.floors.length > 2
-            ? {
-                aspectRatio: "4/3",
-                height: "100dvh",
-                width: "auto",
-              }
-            : {
-                aspectRatio: "8/3",
-                width: "80vw",
-                height: "auto",
-              }
-        }
-      >
-        {map?.floors.map((floor) => (
-          <img
-            key={floor.floor}
-            src={floor.src}
-            alt={floor.floor}
-            className={cn(
-              "object-contain inline w-1/2",
-              map && map.floors.length > 2 && "h-1/2"
-            )}
-          />
-        ))}
+      <div className="flex-1 relative h-screen overflow-hidden py-0 block">
+        <div className="relative h-full w-full flex items-center justify-center">
+          <div
+            className="relative overflow-hidden"
+            style={{
+              aspectRatio: map && map.floors.length > 2 ? "4/3" : "8/3",
+              maxWidth: "100%",
+              maxHeight: "100%",
+              width: "auto",
+              height: "auto",
+            }}
+          >
+            {map?.floors.map((floor) => (
+              <img
+                key={floor.floor}
+                src={floor.src}
+                alt={floor.floor}
+                className={cn(
+                  "object-contain inline w-1/2",
+                  map && map.floors.length > 2 && "h-1/2"
+                )}
+              />
+            ))}
+          </div>
+        </div>
       </div>
 
       {/* Save Button */}
